75eaed39f62cd70f9270da4bb709f0494b346c27
basantpatidar <basantpatidar33@gmail.com>
2020-06-06 23:32:53 -0400

    model

---
+++ src/main/java/com/spring/boot/weather/info/model/WeatherReading.java
@@ -0,0 +1,81 @@
+package com.spring.boot.weather.info.model;
+
+import java.sql.Timestamp;
+
+public class WeatherReading {
+	
+	String id;
+	String city;
+	String description;
+	Double humidity;
+	double pressure;
+	double temperature;
+	Wind wind;
+	Timestamp timestamp;
+	
+	
+	
+	
+	public WeatherReading(String id, String city, String description, Double humidity, double pressure,
+			double temperature, Wind wind, Timestamp timestamp) {
+		super();
+		this.id = id;
+		this.city = city;
+		this.description = description;
+		this.humidity = humidity;
+		this.pressure = pressure;
+		this.temperature = temperature;
+		this.wind = wind;
+		this.timestamp = timestamp;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Double getHumidity() {
+		return humidity;
+	}
+	public void setHumidity(Double humidity) {
+		this.humidity = humidity;
+	}
+	public double getPressure() {
+		return pressure;
+	}
+	public void setPressure(double pressure) {
+		this.pressure = pressure;
+	}
+	public double getTemperature() {
+		return temperature;
+	}
+	public void setTemperature(double temperature) {
+		this.temperature = temperature;
+	}
+	public Wind getWind() {
+		return wind;
+	}
+	public void setWind(Wind wind) {
+		this.wind = wind;
+	}
+	public Timestamp getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Timestamp timestamp) {
+		this.timestamp = timestamp;
+	}
+
+}
