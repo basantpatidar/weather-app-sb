f020f16216de7374ae4778932479f68da13f9d5b
basantpatidar <basantpatidar33@gmail.com>
2020-06-09 13:28:50 -0400

    controller

--- src/main/java/com/spring/boot/weather/info/controller/WeatherInfoController.java
+++ src/main/java/com/spring/boot/weather/info/controller/WeatherInfoController.java
@@ -2,17 +2,48 @@ package com.spring.boot.weather.info.controller;
 
 import javax.inject.Singleton;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.spring.boot.weather.info.model.WeatherReading;
 
 @Singleton
 @RestController
+@RequestMapping("/weather")
 public class WeatherInfoController {
 	
-	@RequestMapping(value="addWeatherReading", method=RequestMethod.POST)
-	public Boolean AddReading() {
-		System.out.println("In");
+	private ObjectMapper objectMapper;
+	//contructor DI
+	public WeatherInfoController(ObjectMapper objectMapper) {
+		this.objectMapper = objectMapper;
+	}
+
+	@GetMapping("/test")
+//	@RequestMapping(value="test", method=RequestMethod.GET)
+	public boolean testEndPoint() {
+		return true;
+	}
+	
+	
+	
+	
+//	@RequestMapping(value="addWeatherReading", method=RequestMethod.POST)
+	@PostMapping("/addWeatherReading")
+	public Boolean AddReading(@RequestBody WeatherReading weatherReading) {
+//	public Boolean AddReading(@RequestBody JsonNode jsonNode) throws JsonMappingException, JsonProcessingException {
+//		WeatherReading weatherReading = objectMapper.readValue(jsonNode.toString(),WeatherReading.class);
+//		System.out.println(jsonNode);
+//		System.out.println(jsonNode.findValue("city"));
+		
+		System.out.println(weatherReading);
+		
 		return true;
 	}
 
