b1205300b342bcde464d64219d9dcb05600d1dba
basantpatidar <basantpatidar33@gmail.com>
2020-06-09 20:12:28 -0400

    wind update

--- src/main/java/com/spring/boot/weather/info/model/Wind.java
+++ src/main/java/com/spring/boot/weather/info/model/Wind.java
@@ -3,5 +3,31 @@ package com.spring.boot.weather.info.model;
 public class Wind {
 
 	double speed;
+	
 	double degree;
+	
+	
+
+	public Wind(double speed, double degree) {
+		super();
+		this.speed = speed;
+		this.degree = degree;
+	}
+	
+	public double getSpeed() {
+		return speed;
+	}
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+	public double getDegree() {
+		return degree;
+	}
+	public void setDegree(double degree) {
+		this.degree = degree;
+	}
+	@Override
+	public String toString() {
+		return "Wind [speed=" + speed + ", degree=" + degree + "]";
+	}
 }
