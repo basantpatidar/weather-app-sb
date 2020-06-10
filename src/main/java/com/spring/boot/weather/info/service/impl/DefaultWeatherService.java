ddf058150cb7c3cc96c8a8bdfee42525874dea20
basantpatidar <basantpatidar33@gmail.com>
2020-06-10 19:25:38 -0400

    other

--- src/main/java/com/spring/boot/weather/info/service/impl/DefaultWeatherService.java
+++ src/main/java/com/spring/boot/weather/info/service/impl/DefaultWeatherService.java
@@ -1,30 +1,38 @@
 package com.spring.boot.weather.info.service.impl;
 
 import java.util.Comparator;
-import java.util.LinkedList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.spring.boot.weather.info.model.WeatherReading;
+import com.spring.boot.weather.info.repository.WeatherRepository;
 import com.spring.boot.weather.info.service.WeatherService;
 
 @Service
 public class DefaultWeatherService implements WeatherService{
 	
-	List<WeatherReading> list = new LinkedList<>();
+//	List<WeatherReading> list = new LinkedList<>();
+	
+	WeatherRepository weatherRespository;
+	
+	public DefaultWeatherService(WeatherRepository weatherRepository) {
+		this.weatherRespository = weatherRepository;
+	}
 
 	@Override
 	public boolean addWeatherReadings(WeatherReading weatherReading) {
 		System.out.println(weatherReading);
-		list.add(weatherReading);
+//		list.add(weatherReading);
+		weatherRespository.save(weatherReading);
 		return true;
 	}
 
 	@Override
-	public List<WeatherReading> getWeatherReadings() {
-		list.sort(Comparator.comparing(WeatherReading::getTimestamp));
-		return list;
+	public List<WeatherReading> getWeatherReadingsSorted() {
+//		list.sort(Comparator.comparing(WeatherReading::getTimestamp));
+		List<WeatherReading> weatherList = (List<WeatherReading>) weatherRespository.findAll();
+		return weatherList;
 	}
 
 	
