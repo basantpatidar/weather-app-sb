49089a0e5273cda99bc5d35554bfc22fd19081bb
basantpatidar <basantpatidar33@gmail.com>
2020-06-23 12:22:59 -0400

    sns major development

--- src/main/java/com/spring/boot/weather/info/config/MyWebMvcConfig.java
+++ src/main/java/com/spring/boot/weather/info/config/MyWebMvcConfig.java
@@ -1,10 +1,13 @@
 package com.spring.boot.weather.info.config;
 
+import org.springframework.cloud.aws.messaging.core.NotificationMessagingTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.amazonaws.services.sns.AmazonSNS;
 
 @Configuration
 public class MyWebMvcConfig {
@@ -23,6 +26,11 @@ public class MyWebMvcConfig {
 	@Bean
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
+	}
+	
+	@Bean
+	public NotificationMessagingTemplate notificationMessagingTemplate(AmazonSNS amazonSNS) {
+		return new NotificationMessagingTemplate(amazonSNS);
 	}
 
 }
--- src/main/java/com/spring/boot/weather/info/service/impl/DefaultWeatherService.java
+++ src/main/java/com/spring/boot/weather/info/service/impl/DefaultWeatherService.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.spring.boot.weather.info.awsMessanging.WeatherAlertSns;
 import com.spring.boot.weather.info.model.WeatherAlert;
 import com.spring.boot.weather.info.model.WeatherReading;
 import com.spring.boot.weather.info.service.WeatherService;
@@ -16,24 +19,32 @@ public class DefaultWeatherService implements WeatherService{
 //	List<WeatherReading> list = new LinkedList<>();
 	
 	private RestTemplate restTemplate;
+	private WeatherAlertSns weatherAlertSns;
+	private ObjectMapper objectMapper;
 	
 	@Autowired
-	public DefaultWeatherService(RestTemplate restTemplate) {
+	public DefaultWeatherService(RestTemplate restTemplate, WeatherAlertSns weatherAlertSns, ObjectMapper objectMapper) {
 		this.restTemplate = restTemplate;
+		this.weatherAlertSns = weatherAlertSns;
+		this.objectMapper = objectMapper;
 	}
 
 	@Override
-	public boolean addWeatherReadings(WeatherReading weatherReading) {
+	public boolean addWeatherReadings(WeatherReading weatherReading) throws JsonProcessingException {
 		System.out.println(weatherReading);
 //		list.add(weatherReading);
 		if(weatherReading.getTemperature() > 25) {
 			WeatherAlert weatherAlert = new WeatherAlert("Too Hot", weatherReading);
-			return restTemplate.postForObject("http://localhost:9090/addReading", weatherAlert, boolean.class);
+			String message = objectMapper.writeValueAsString(weatherAlert);
+			weatherAlertSns.send("Tempareture", message);
+//			return restTemplate.postForObject("http://localhost:9090/addReading", weatherAlert, boolean.class);
 		}
 		
 		if(weatherReading.getWind().getSpeed() > 6) {
 			WeatherAlert weatherAlert = new WeatherAlert("Too windy", weatherReading);
-			return restTemplate.postForObject("http://localhost:9090/addReading", weatherAlert, boolean.class);
+			String message = objectMapper.writeValueAsString(weatherAlert);
+			weatherAlertSns.send("Tempareture", message);
+//			return restTemplate.postForObject("http://localhost:9090/addReading", weatherAlert, boolean.class);
 		}
 		return false;
 	}
