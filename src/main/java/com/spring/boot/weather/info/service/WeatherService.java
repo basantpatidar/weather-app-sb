55bc73281418a21a2a857e1455218be5d8c93d66
basantpatidar <basantpatidar33@gmail.com>
2020-06-23 12:21:30 -0400

    exception handling for jackson

--- src/main/java/com/spring/boot/weather/info/service/WeatherService.java
+++ src/main/java/com/spring/boot/weather/info/service/WeatherService.java
@@ -2,11 +2,12 @@ package com.spring.boot.weather.info.service;
 
 import java.util.List;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.spring.boot.weather.info.model.WeatherReading;
 
 public interface WeatherService {
 
-	boolean addWeatherReadings(WeatherReading weatherReading);
+	boolean addWeatherReadings(WeatherReading weatherReading) throws JsonProcessingException;
 	List<WeatherReading> getWeatherReadingsSorted();
 	
 }
